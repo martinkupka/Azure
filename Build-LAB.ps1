fbb1d0047dd4aed52a11604c2c2d66bfd482c77b
martinkupka <martin@kupka.me>
2018-10-27 10:19:35 +0200

    Testing the GIT commit

--- Build-LAB.ps1
+++ Build-LAB.ps1
@@ -3,13 +3,16 @@ if (!(Get-Module -Name *AzureRM*)) {
 
     # Install the module if it isn't found within available modules
     if (!(Get-Module -Name *AzureRM* -ListAvailable)) {
+        Write-Host "Installing AzureRM module" -ForegroundColor Green
         Install-Module -Name AzureRM -AllowClobber -Force
     }
 
     # Import the module
+    Write-Host "Importing AzureRM module" -ForegroundColor Green
     Import-Module -Name AzureRM
 }
 
 # Connect and sign in to AzureRM
+Write-Host "Connecting to AzureRM" -ForegroundColor Green
 Connect-AzureRmAccount
 
