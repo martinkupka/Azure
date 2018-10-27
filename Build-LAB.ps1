f860c9a85f4351caf733d360ae36db06e90da80c
martinkupka <martin@kupka.me>
2018-10-28 01:24:02 +0200

    First functional outline - VM is up and running!

--- Build-LAB.ps1
+++ Build-LAB.ps1
@@ -16,7 +16,21 @@ $VNSubnetAddressPrefix = "10.0.1.0/24"
 $IPName = "publicIP1"
 
 $NICName = "NIC1"
-$IPConfigName = "NIC1_PublicIPConfig1"
+
+$NSGroupName = "NSGroup1"
+$NSGroupRuleName = "NSFrontEndSubnet"
+$NSGroupRuleDesc = "Allows RDP connection on port 3389"
+
+$SourcePort = "*"
+$DestinationPort = "3389"
+$SourceIP = "92.105.180.100"
+$DestinationIP = "*"
+$Priority = 1000
+
+$VMName = "VM-LAB1"
+$VMLocalAdminUser = "LocalAdminUser"
+$VMLocalAdminSecurePassword = ConvertTo-SecureString 'Pa$$w0rd' -AsPlainText -Force
+$VMSize = "Standard_D2s_v3"
 
 # Check if AzureRM module is installed and present
 if (!(Get-Module -Name *AzureRM*)) {
@@ -51,33 +65,87 @@ New-AzureRmStorageAccount `
     -SkuName $StoragePerfRep `
     -Verbose
 
+# Set up Security Group on Subnet and allow RDP on 3389
+New-AzureRmNetworkSecurityRuleConfig `
+    -Name $NSGroupRuleName `
+    -Description $NSGroupRuleDesc `
+    -Protocol * `
+    -SourcePortRange $SourcePort `
+    -DestinationPortRange $DestinationPort `
+    -SourceAddressPrefix $SourceIP `
+    -DestinationAddressPrefix $DestinationIP `
+    -Access Allow `
+    -Direction Inbound `
+    -priority $Priority `
+    -OutVariable SecurityRule
+
+New-AzureRmNetworkSecurityGroup `
+    -Name $NSGroupName `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+    -SecurityRules $SecurityRule `
+    -OutVariable SecurityGroup
+
 # Create Virtual Network
 $DefaultSubnet = New-AzureRmVirtualNetworkSubnetConfig `
--Name $VNSubnetName `
--AddressPrefix $VNSubnetAddressPrefix `
--Verbose
+    -Name $VNSubnetName `
+    -AddressPrefix $VNSubnetAddressPrefix `
+    -NetworkSecurityGroup $SecurityGroup `
+    -Verbose
 
 New-AzureRmVirtualNetwork `
--Name $VNName `
--ResourceGroupName $ResourceGroupName `
--Location $ResourceGroupLocation `
--Subnet $DefaultSubnet `
--AddressPrefix $VNAddressPrefix `
--OutVariable VirtualNetwork `
--Verbose
+    -Name $VNName `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+    -Subnet $DefaultSubnet `
+    -AddressPrefix $VNAddressPrefix `
+    -OutVariable VirtualNetwork `
+    -Verbose
 
-$PublicIP = New-AzureRmPublicIpAddress `
--Name $IPName `
--ResourceGroupName $ResourceGroupName `
--Location $ResourceGroupLocation `
--AllocationMethod Dynamic `
+New-AzureRmPublicIpAddress `
+    -Name $IPName `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+    -AllocationMethod Dynamic `
+    -OutVariable PublicIP
 
 $virtualsubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork
 
 New-AzureRmNetworkInterface `
--Name $NICName `
--ResourceGroupName $ResourceGroupName `
--Location $ResourceGroupLocation `
--Subnet $VirtualSubnet
+    -Name $NICName `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+    -Subnet $VirtualSubnet `
+    -PublicIpAddress $PublicIP
 
 # Create VM
+$Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
+$NIC = Get-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName
+
+$VirtualMachine = New-AzureRmVMConfig `
+    -VMName $VMName `
+    -VMSize $VMSize
+
+$VirtualMachine = Set-AzureRmVMOperatingSystem `
+    -VM $VirtualMachine `
+    -Windows `
+    -ComputerName $VMName `
+    -Credential $Credential `
+    -ProvisionVMAgent `
+    -EnableAutoUpdate
+
+$VirtualMachine = Add-AzureRmVMNetworkInterface `
+    -VM $VirtualMachine `
+    -Id $NIC.Id
+
+$VirtualMachine = Set-AzureRmVMSourceImage `
+    -VM $VirtualMachine `
+    -PublisherName 'MicrosoftWindowsServer' `
+    -Offer 'WindowsServer' `
+    -Skus  "2016-Datacenter" `
+    -Version latest
+
+New-AzureRmVm `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+    -VM $VirtualMachine
--- RandomTrainingStuff.ps1
+++ RandomTrainingStuff.ps1
@@ -9,3 +9,50 @@ Get-AzureRmResourceGroup | Where-Object ResourceGroupName -notlike *RG* | Remove
 
 # Get VN object
 Get-AzureRmVirtualNetwork | Where-Object Name -eq $VNName
+
+# Changing existing security rule example
+Get-AzureRmNetworkSecurityGroup `
+    -Name $NSGroupName `
+    -ResourceGroupName $ResourceGroupName `
+| Set-AzureRmNetworkSecurityRuleConfig `
+    -Name $NSGroupRuleName `
+    -SourceAddressPrefix "92.105.180.100" `
+    -Protocol * `
+    -Access Allow `
+    -Direction Inbound `
+    -SourcePortRange * `
+    -DestinationPortRange "3389" `
+    -DestinationAddressPrefix * `
+    -Priority 1000 `
+| Set-AzureRmNetworkSecurityGroup
+
+###################################################################################
+# Get Virtual Network from Azure
+$VirtualNetwork = Get-AzureRmVirtualNetwork | Where-Object Name -eq $VNName
+
+# Modify the subnet
+Set-AzureRmVirtualNetworkSubnetConfig `
+    -Name $VNSubnetName `
+    -AddressPrefix $VNSubnetAddressPrefix `
+    -NetworkSecurityGroup $SecurityGroup `
+    -VirtualNetwork $VirtualNetwork
+
+# Commit the change
+$virtualnetwork | Set-AzureRmVirtualNetwork
+#################################################################################
+
+# https://docs.microsoft.com/en-us/azure/virtual-network/virtual-network-multiple-ip-addresses-powershell
+
+#################################################################################
+# Setting Public IP on already existing NIC
+$NIC = Get-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName
+
+# Grab public IP from Azure
+$PublicIP = Get-AzureRmPublicIpAddress
+
+# Assign to NIC
+$nic.IpConfigurations[0].PublicIpAddress = $publicip
+
+# Commit
+$nic | Set-AzureRmNetworkInterface
+#################################################################################
